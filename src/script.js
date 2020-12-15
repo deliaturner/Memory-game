c7e55bc76a787077e98be4ceafbdb8b183056d23
bshellenbarger <71110804+bshellenbarger@users.noreply.github.com>
2020-12-14 20:43:26 -0500

    brit's changes

--- src/script.js
+++ src/script.js
@@ -3,13 +3,46 @@ const gameBoard = document.querySelector(".memory-game");
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
-// User will open game and select a card to be flipped
-// ADDED cannot click same card to compare
 let clickedCardCount = 0;
+let matchedPairs = 0;
+let startPressed = false;
+// deal cards upon page load
+window.addEventListener("DOMContentLoaded", (event) => {
+  // console.log(cards);
+  newDeal();
+});
+// Reset and Start Button will flip all cards back-face, reset all the variables, shuffle,
+// append to screen, and start the timer at zero
+document.getElementById("resetbutton").addEventListener("click", reset);
+document.getElementById("startbutton").addEventListener("click", startButtonPress);
+function startTimer() {
+  timer.innerHTML = `${minute}mins ${second}secs`; //display on screen
+  second++;
+  if (second === 60) {
+    minute++;
+    second = 0;
+  }
+}
+// timer display
+const timer = document.getElementById("timerbutton");
+let second = 0; // set intial time to zero
+let minute = 0;
+timer.innerHTML = `${minute}mins ${second}secs`;
+let interval;
+function startButtonPress() {
+  if (!startPressed) {
+    interval = setInterval(startTimer, 1000); // do the startTimer function every 1 second (1000ms)
+    // run flipCard function upon card click
+    cards.forEach((card) => card.addEventListener("click", flipCard));
+    startPressed = true;
+  }
+}
+// flip card by toggling hidden class on images
+// check for match when two cards are flipped up
 function flipCard() {
   // console.log(this.childNodes);
   // console.log("flip the card");
-  if (lockBoard) return; //if locked, don't do anything
+  if (lockBoard) return; // if locked, don't do anything
   clickedCardCount++;
   if (clickedCardCount === 1) {
     this.childNodes[3].classList.toggle("hidden");
@@ -21,28 +54,18 @@ function flipCard() {
       this.childNodes[3].classList.toggle("hidden");
       this.childNodes[1].classList.toggle("hidden");
       secondCard = this;
-      checkForMatch();
       clickedCardCount = 0;
+      checkForMatch();
     }
   }
-  // if (!hasFlippedCard) {
-  //   // First Card Clicked
-  //   hasFlippedCard = true;
-  //   firstCard = this;
-  //   // Second Card Clicked
-  // } else {
-  //   hasFlippedCard = false;
-  //   secondCard = this;
-  //   checkForMatch();
-  // }
 }
-// Do the cards Match?
-let matchedPairs = 0;
+// check for card match
+// display message to user if game is won
 function checkForMatch() {
-  console.log(firstCard, secondCard);
+  // console.log(firstCard, secondCard);
   let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
   if (isMatch) {
-    console.log("cards match");
+    // console.log("cards match");
     setTimeout(removeCardsFromGame, 1000); //display for 1 sec before removing
     matchedPairs++;
     if (matchedPairs === 10) {
@@ -53,20 +76,7 @@ function checkForMatch() {
     unflipCards();
   }
 }
-// remove cards from game
-function removeCardsFromGame() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
-  firstCard.style.display = "hidden";
-  secondCard.style.display = "hidden";
-  firstCard.childNodes[3].style.display = "none";
-  firstCard.childNodes[1].style.display = "none";
-  secondCard.childNodes[3].style.display = "none";
-  secondCard.childNodes[1].style.display = "none";
-}
-// Disable(Stop) cards from being moved
-// function disableCards() {}
-//Cards will stay face up
+// flip cards facedown by toggling hidden class on images
 function unflipCards() {
   // console.log("not a match");
   lockBoard = true; // cannot click more cards
@@ -75,79 +85,64 @@ function unflipCards() {
     firstCard.childNodes[1].classList.toggle("hidden");
     secondCard.childNodes[3].classList.toggle("hidden");
     secondCard.childNodes[1].classList.toggle("hidden");
+    lockBoard = false; // allow clicking cards again
   }, 1000);
-  // Check for setTimeout on how to have cards flip back facedown automatically when it is not a matched pair.
-  setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
-    lockBoard = false; //allow clicking cards again
-  }, 2000);
 }
-
-// Timer display
-const timer = document.getElementById("timerbutton");
-let interval = setInterval(startTimer, 1000); //do the startTimer function every 1 second (1000ms)
-let second = 0; //set intial time to zero
-let minute = 0;
-function startTimer() {
-  timer.innerHTML = `${minute}mins ${second}secs`; //display on screen
-  second++;
-  if (second == 60) {
-    minute++;
-    second = 0;
-  }
+// remove cards from game by adding hidden class to images
+function removeCardsFromGame() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  firstCard.childNodes[3].classList.add("hidden");
+  firstCard.childNodes[1].classList.add("hidden");
+  secondCard.childNodes[3].classList.add("hidden");
+  secondCard.childNodes[1].classList.add("hidden");
 }
-
-// Reset the Timer
-function resetTimer() {
-  clearInterval(interval); //stop the timer
-  second = 0; // reset to zero
-  minute = 0;
-  setInterval(startTimer, 1000); //start the timer again
-}
-
-// Reset and Start Button will flip all cards back-face, reset all the variables, shuffle, append to screen, and start the timer at zero
-document.getElementById("startbutton").addEventListener("click", startAndReset);
-document.getElementById("resetbutton").addEventListener("click", startAndReset);
-function startAndReset() {
+// restore the game board for a new game
+// runs on click of Start or Reset buttons
+function reset() {
   for (let card of cards) {
-    //display all card backs
-    card.style.display = "inline-block"; //display all cards
-    card.childNodes[3].style.display = "inline-block"; //display backs
-    card.childNodes[1].classList.add("hidden"); //hide fronts
+    card.style.display = "block"; // display all cards
+    card.childNodes[1].classList.add("hidden"); // apply hidden class to fronts
+    card.childNodes[3].classList.remove("hidden"); // remove hidden class from backs
+    card.removeEventListener("click", flipCard);
   }
-  resetBoard(); //resets the card variables
-  newDeal(); //shuffle and add to screen
-  resetTimer(); //stops and starts the timer at zero
+  resetBoard(); // resets the card variables
+  newDeal(); // shuffle and add to screen
+  resetTimer(); // stops and starts the timer at zero
 }
-
-// Board is reset to all cards being on back face
+// reset all variables
 function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+  hasFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+  clickedCardCount = 0;
+  matchedPairs = 0;
+  startPressed = false;
 }
-// cards will be shuffled each time page is reloaded
+// shuffle the cards
 function shuffle() {
   for (i = 0; i < cards.length; i++) {
     let randomPos = Math.floor(Math.random() * 20);
     let randomCard = cards[randomPos];
     cards[randomPos] = cards[i];
     cards[i] = randomCard;
-    console.log(randomCard, cards[i], cards);
+    // console.log(randomCard, cards[i], cards);
   }
-  console.log(cards);
+  // console.log(cards);
 }
-
+// shuffle cards and append to gameBoard
 function newDeal() {
-  //put cards on the screen
   gameBoard.innerHTML = "";
   shuffle();
   cards.forEach((card) => {
     gameBoard.appendChild(card);
   });
 }
-window.addEventListener("DOMContentLoaded", (event) => {
-  console.log(cards);
-  newDeal();
-});
-cards.forEach((card) => card.addEventListener("click", flipCard));
+// reset the timer
+function resetTimer() {
+  clearInterval(interval); // stop the timer
+  second = 0; // reset to zero
+  minute = 0;
+  timer.innerHTML = `${minute}mins ${second}secs`; //display on screen
+}
